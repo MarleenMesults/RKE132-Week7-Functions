cd5d4b810f2d63b620b721fe50794f5028f91bd8
Marleen Mesults <marleen.mesults@tktk.ee>
2023-03-02 14:24:46 +0200

    create new app

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("How many steps have you walked today?");
+int userSteps = Int32.Parse(Console.ReadLine());
+
+double result = HealthMonitor(userSteps);
+Console.WriteLine(userSteps);
+
+static void HealthMonitor(int steps)
+{
+    if (steps < 4000)
+    {
+
+        Console.WriteLine("You need to move more.");
+    }
+    else if (steps >= 4000 && steps < 8000)
+    {
+
+        Console.WriteLine("Health goal achieved,");
+    }
+    else
+    {
+
+        Console.WriteLine("Well done!");
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿
+Console.WriteLine("Enter the operation (+ or - or * or /):");
+char userOperator = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a:");
+int firstNum = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter b:");
+int secondNum = Int32.Parse(Console.ReadLine());
+
+switch (userOperator)
+{
+
+    case '+':
+        Addition(firstNum, secondNum);
+        break;
+    case '-':
+        subtraction(firstNum, secondNum);
+        break;
+    case '*':
+        multiplication(firstNum, secondNum);
+        break;
+    case '/':
+        divided(firstNum, secondNum);
+        break;
+    default:
+        Console.WriteLine("invalid operator");
+        break;
+}
+static void Addition(int a, int b)
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+
+static void subtraction(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+}
+
+static void multiplication(int a, int b)
+{
+    Console.WriteLine($"{a} * {b} = {a * b}");
+}
+
+static void divided(int a, int b)
+{
+    Console.WriteLine($"{a} / {b} = {a / b}");
+}
